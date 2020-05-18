283acb9be685eecadafc12e686d82817efd50e86
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-05-17 21:20:00 -0500

    skills component

--- src/Components/skills/Skills.js
+++ src/Components/skills/Skills.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './skills.css'
-import Graduate from '../images/woman.png'
 
 
 
@@ -10,11 +9,66 @@ export default function Skills() {
             <div>
                 <h3>My Skills</h3>
             </div>
-            <div>
-                <img src={Graduate} />
+            <div className="higher-ed">
+                <h5>Higher Education:</h5>
+                <ul>
+                    <li className="graduate">Bachelor's degree in Mathematics and Computer Applications.</li>
+                </ul>
             </div>
+            <h5 className="tech-skills-h5"> Technical Skills: </h5>
+            <div className="full-stack">
+                <div className="skills-box">
+                    <h4>Front End</h4>
+                    <ul className="dev-skills">
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Redux</li>
+                    </ul>
+                </div>
+                <div className="skills-box">
+                    <h4>Back End</h4>
+                    <ul className="dev-skills">
+                        <li className="lambda">
+                            Python</li>
+                        <li className="lambda">NodeJs</li>
+                        <li className="lambda">
+                            Express NodeJs</li>
+                        <li className="lambda">
+                            Knex</li>
+                        <li className="lambda">
+                            SQLite 3</li>
+                        <li className="lambda">
+                            PostgreSQL</li>
+                        <li className="lambda">
+                            My SQL</li>
+                    </ul>
+                </div>
+                <div className="skills-box">
+                    <h4>Others</h4>
+                    <ul className="dev-skills">
+                        <li>npm</li>
+                        <li>Git / Github</li>
+                        <li> Deployment Sites</li>
+                        <ul className="dev-skills">
+                            <li className="lambda">
+                                Heroku
+                            </li>
+                            <li className="lambda">
+                                Vercel
+                            </li>
+                            <li className="lambda">
+                                Netlify
+                            </li>
+                        </ul>
 
-
-        </div>
+                    </ul>
+                </div>
+            </div>
+        </div >
     )
 }
+
+
+// <a target="_blank" href="https://icons8.com/icons/set/postgreesql">PostgreSQL icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
