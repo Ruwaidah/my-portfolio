34cf1964cfd33d4a70029293c6e9bd93da8ee002
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-05-18 20:04:12 -0500

    start with project component

--- src/Components/projects/Projects.js
+++ src/Components/projects/Projects.js
@@ -1,28 +1,107 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './projects.css'
 import Refugee from '../images/refugee.png'
 
 
 export default function Projects() {
+    let val = 0;
+    for (let i = 0; i < 10; i++) {
+        setTimeout(() => {
+            console.log(val += 1)
+        }, 1000)
+    }
+
     return (
         <div className="projects-com">
-            <div className="first-proj">
-                <div className="image-title">
-                    <a
-                        href="https://frosty-galileo-c1f29c.netlify.com/index.html"
-                        target="_blank"
-                    >
-                        {" "}
-                        <img id="projectImage" src={Refugee} />
-                    </a>
-                    <p className="project-title">Refugee Stories </p>
+            <div className="projects-header">
+                <h5>Projects at Lambda School </h5>
+            </div>
+            <div id="slideshow">
+                <div class="slide-wrapper">
+                    <div class="slide">
+                        <div className="image-title">
+                            <a
+                                href="https://frosty-galileo-c1f29c.netlify.com/index.html"
+                                target="_blank"
+                            >
+                                {" "}
+                                <img id="projectImage" src={Refugee} />
+                            </a>
+                            <p className="project-title">Refugee Stories </p>
+                        </div>
+                        <ul className="dev-skills">
+                            <li>Semantic html.</li>
+                            <li>Style with Css.</li>
+                            <li>Media queries for fully responsive web design. </li>
+                            <li>Some javaScipt in the about page.</li>
+                        </ul>
+                    </div>
+                    <div class="slide">
+                        <div className="image-title">
+                            <a
+                                href="https://frosty-galileo-c1f29c.netlify.com/index.html"
+                                target="_blank"
+                            >
+                                {" "}
+                                <img id="projectImage" src={Refugee} />
+                            </a>
+                            <p className="project-title">Refugee Stories </p>
+                        </div>
+                        <ul className="dev-skills">
+                            <li>Semantic html.</li>
+                            <li>Style with Css.</li>
+                            <li>Media queries for fully responsive web design. </li>
+                            <li>Some javaScipt in the about page.</li>
+                        </ul>
+                    </div>
+                    <div class="slide">
+                        <div className="image-title">
+                            <a
+                                href="https://frosty-galileo-c1f29c.netlify.com/index.html"
+                                target="_blank"
+                            >
+                                {" "}
+                                <img id="projectImage" src={Refugee} />
+                            </a>
+                            <p className="project-title">Refugee Stories </p>
+                        </div>
+                        <ul className="dev-skills">
+                            <li>Semantic html.</li>
+                            <li>Style with Css.</li>
+                            <li>Media queries for fully responsive web design. </li>
+                            <li>Some javaScipt in the about page.</li>
+                        </ul>
+                    </div>
+                    <div class="slide">
+                        <div className="image-title">
+                            <a
+                                href="https://frosty-galileo-c1f29c.netlify.com/index.html"
+                                target="_blank"
+                            >
+                                {" "}
+                                <img id="projectImage" src={Refugee} />
+                            </a>
+                            <p className="project-title">Refugee Stories </p>
+                        </div>
+                        <ul className="dev-skills">
+                            <li>Semantic html.</li>
+                            <li>Style with Css.</li>
+                            <li>Media queries for fully responsive web design. </li>
+                            <li>Some javaScipt in the about page.</li>
+                        </ul>
+                    </div>
                 </div>
-                <ul>
-                    <li>Use semantic html.</li>
-                    <li>Style with Css and some javaScipt. </li>
-                    <li>Media queries for fully responsive web design. </li>
-                </ul>
+            </div>
+            <div className="dot-btns">
+                <div className="pro-btn active-btn"></div>
+                <div className="pro-btn"></div>
+                <div className="pro-btn"></div>
+                <div className="pro-btn"></div>
+                <div className="pro-btn"></div>
             </div>
         </div>
     )
 }
+
+
+
