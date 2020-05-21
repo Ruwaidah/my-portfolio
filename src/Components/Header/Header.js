a313d07d24662f2778311ace75b1b103d4af1103
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-05-21 14:26:25 -0500

    responsive contact page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,34 +1,43 @@
-import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 export default function Header(props) {
-    return (
-        <div className="header-com">
-            <div className="myname-header">
-                <h3>Ruwaidah Alfakhri</h3>
-                <h6>Full Stack Web Developer</h6>
-            </div>
-            <div className="nav-links">
-                <div className={`line ${props.location.pathname === "/" ? "home-line" : props.location.pathname === "/projects" ?
-                    "projects" :
-                    props.location.pathname === "/about-me" ? "aboutme" :
-                        props.location.pathname === "/contact-me" ? "contactme" : props.location.pathname === "/skills" ? "skills" : null
-                    }`}></div>
-                <div className="aboutme-link">
-                    <Link to="/about-me">About Me</Link>
-                </div>
-                <div className="skils-link">
-                    <Link to="/skills">Skills</Link>
-                </div>
-                <div className="projects-link">
-                    <Link to="/projects">Projects</Link>
-                </div>
-                <div className="contactme-link">
-                    <Link to="/contact-me">Contact Me</Link>
-                </div>
-            </div>
+  return (
+    <div className="header-com">
+      <div className="myname-header">
+        <h3>Ruwaidah Alfakhri</h3>
+        <h6>Full Stack Web Developer</h6>
+      </div>
+      <div className="nav-links">
+        <div
+          className={`line ${
+            props.location.pathname === "/"
+              ? "home-line"
+              : props.location.pathname === "/projects"
+              ? "projects"
+              : props.location.pathname === "/about-me"
+              ? "aboutme"
+              : props.location.pathname === "/contact-me"
+              ? "contactme"
+              : props.location.pathname === "/skills"
+              ? "skills"
+              : null
+          }`}
+        ></div>
+        <div className="aboutme-link">
+          <Link to="/about-me">About</Link>
         </div>
-    )
+        <div className="skils-link">
+          <Link to="/skills">Skills</Link>
+        </div>
+        <div className="projects-link">
+          <Link to="/projects">Projects</Link>
+        </div>
+        <div className="contactme-link">
+          <Link to="/contact-me">Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
