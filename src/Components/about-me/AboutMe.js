84b84a94e60f4158898b11e3abe498c09eb02411
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-05-22 16:47:51 -0500

    fix margin in project page

--- src/Components/about-me/AboutMe.js
+++ src/Components/about-me/AboutMe.js
@@ -14,8 +14,8 @@ export default function AboutMe() {
           <img src={Image} className="my-image" alt="ruwaidah-img" />
           Hi, I'm Ruwaidah, I'm a Full Stuck Web Developer and I have a
           bachelor's degree in Mathematics and Computer Science as I took the
-          4th place of my cohort so you can tell I enjoy solving problems, I"m
-          life- long learner. I have learning agility and take what I have
+          4th place of my cohort so you can tell I enjoy solving problems, I'm
+          life long learner. I have learning agility and take what I have
           learned in the real world.
         </p>
       </div>
